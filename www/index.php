045ca758d2573fde6fa822e8d13c67c6cb43d770
martiniden148@yandex.ru <martiniden148@yandex.ru>
2022-12-08 01:50:18 +0300

    add bat files

---
+++ www/index.php
@@ -0,0 +1,18 @@
+<?
+echo 'Docker Test' . PHP_EOL;
+//phpinfo();
+//$hostname берется из имени контейнера мускулов
+//$conn = mysqli_connect('mysql', 'root', 'root');
+//try { $dbConn = new PDO(
+//    'mysql:host=mysql;',
+//    'root',
+//    'mighty');
+//} catch (Exception $e) {
+//    $e->getMessage() ;
+//}
+//$conn->query('CREATE DATABASE `test-docker1`');
+//$conn->query('show databases');
+
+echo '<pre>';
+//var_dump($conn->get_server_info());
+?>
---
+++ www/test-app/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marti
+ * Date: 30.11.2022
+ * Time: 0:41
+ */
+
+echo 'test-app here!';
